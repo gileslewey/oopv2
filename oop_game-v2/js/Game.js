3aa29a3f8b7931664d104769ffd5199a0ecd3928
gileslewey <35309503+gileslewey@users.noreply.github.com>
2022-03-16 19:06:52 -0500

    console error after reset

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -5,7 +5,7 @@
  class Game {
 
   constructor(){
-  this.missed = 0;
+   this.missed = 0;
 
    this.phrases = [
    new Phrase('May the force be with you'),
@@ -14,10 +14,65 @@
    new Phrase('You cant handle the truth'),
    new Phrase('Houston we have a problem')
    ];
-
   this.activePhrase = null;
   }
 
+
+  handleInteraction(){
+    console.log('handle interaction');
+    keyStrokes.addEventListener('click', (event) => {
+    let keyStroke = event.target.textContent;
+    if (this.activePhrase.checkLetter(keyStroke) === true) {
+    this.activePhrase.showMatchedLetter(keyStroke);
+    event.target.className = 'chosen';
+    event.target.disabled = true;
+    } else {this.removeLife()
+    event.target.className = 'wrong'
+    event.target.disabled = true;
+    } if (this.checkForWin()){
+      this.gameOver(true);
+    } if (this.missed > 4){
+      this.gameOver(false)
+      }
+    })
+  };
+
+  removeLife() {
+        this.missed += 1
+        let missed = this.missed;
+        document.querySelector(`#scoreboard ol :nth-child(${missed})`).firstChild.src = 'images/lostHeart.png'
+    };
+
+  checkForWin() {
+    let phraseLength = this.activePhrase.phrase.split(' ').join('');
+    let showLength = document.getElementsByClassName('show letter');
+    if (showLength.length === phraseLength.length){
+      return true;
+      } else {
+      return false;
+    }
+};
+
+
+  gameOver(gameWon){
+    if (this.checkForWin() === true){
+      overlay.className = 'win';
+      console.log('win');
+      gameOverMessage.innerHTML = 'You Win!'
+      keyRows.style.display = 'none';
+      overlay.style.display = 'block';
+      this.resetGame();
+      console.log('Win');
+    } else {
+      overlay.className = 'lose';
+      gameOverMessage.innerHTML = 'You Lose'
+      keyRows.style.display = 'none';
+      overlay.style.display = 'block';
+      this.resetGame();
+      console.log('Lose');
+    }
+
+  }
   /**
   * Begins game by selecting a random phrase and displaying it to user
   */
@@ -28,7 +83,19 @@
     this.activePhrase.addPhraseToDisplay();
   };
 
-
+  resetGame(){
+      this.activePhrase = null;
+      this.missed = 0;
+      textSpot.innerHTML = ''
+      for (let i = 0; i < buttons.length; i++){
+              buttons[i].className = 'key'
+              buttons[i].disabled = false;
+      }
+      for (let i=1; i<lives.length +1; i++) {
+        document.querySelector(`#scoreboard ol :nth-child(${i})`).firstChild.src = 'images/liveHeart.png'
+      }
+      keyRows.style.display = 'block';
+  }
  /**
  * Selects random phrase from phrases property
  * @return {Object} Phrase object chosen to be used
@@ -37,13 +104,5 @@
     let newPhrase = this.phrases;
     return newPhrase[Math.floor(Math.random()*newPhrase.length)];
   };
-
-  handleInteraction(){}
-
-  removeLife(){}
-
-  checkForWin(){}
-
-  gameOver(){}
-
+//maybe phrases is why I get it 5*
  }
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,62 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
-const game = new Game;
+const keyStrokes = document.getElementById('qwerty');
 const startButton = document.getElementById('btn__reset');
+const overlay = document.getElementById('overlay');
+const gameOverMessage = document.getElementById('game-over-message');
+const keyRows = document.getElementById('qwerty');
+const textSpot = document.getElementById('phrase').getElementsByTagName('ul')[0];
+const buttons = document.querySelectorAll('button');
+const lives = document.querySelectorAll('li.tries');
+console.log(lives);
+
 startButton.addEventListener('click', function(){
+game = new Game();
 game.startGame();
+game.handleInteraction();
 });
 
-class App {
-constructor(){
-
-
-// {
-// new Game();
-// console.log('game started');
-// };
-
-
-
-
-
-   }
- }
-
- // const logPhrase = (phrase) => {
- // console.log(`Phrase - phrase: `, phrase.phrase);
- // };
- // const game = new Game();
- // console.log(game.getRandomPhrase());
- // logPhrase(game.getRandomPhrase());
- // logPhrase(game.getRandomPhrase());
- // logPhrase(game.getRandomPhrase());
- // logPhrase(game.getRandomPhrase());
- // logPhrase(game.getRandomPhrase());
-
-
-// const game = new Game();
-// const overlay = document.getElementById('overlay');
-// overlay.style.display = 'none';
-// const randPhrase = game.getRandomPhrase();
-// randArr = randPhrase.phrase.split("");
-// const map1 = randArr.map( function (letter) {
-//   if (letter === ' '){
-//     return `<li class="space">${letter}</li>`
-//   } else { return `<li class="hide letter ${letter}">${letter}</li>`;
+//document.querySelector(`#scoreboard ol :nth-child(${i})`).firstChild.src = 'images/lostHeart.png'
+// for (let i=0; i<hearts.length; i++) {
+//   document.querySelector(`#scoreboard ol :nth-child(${i})`).firstChild.src = 'images/liveHeart.png';
 // }
-// });
-// let noCommas = map1.join('');
-// textSpot = document.getElementById('phrase').getElementsByTagName('ul')[0];
-// textSpot.textContent = '';
-// textSpot.insertAdjacentHTML("afterbegin", noCommas);
 
-// const game = new Game();
-// game.startGame();
-// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
- // const myArray = text.split(" ");
- // let word = myArray[1];
- // console.log(word);
- // // const phrase = new Phrase('Life is like a box of chocolates');
- // console.log(`Phrase - phrase: ${phrase.phrase}`);
+// for (let i=1; i<lives.length +1; i++) {
+//   document.querySelector(`#scoreboard ol :nth-child(${i})`).firstChild.src = 'images/lostHeart.png'
+// }
