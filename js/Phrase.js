2623e0b032c3a3ce73d5a48d4e7534e1ce15e04b
gileslewey <35309503+gileslewey@users.noreply.github.com>
2022-03-16 19:04:02 -0500

    oop 4
    ss

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,36 +5,47 @@
  class Phrase{
 
 constructor(phrase){
-  console.log('hi there ');
+  console.log('phrase constructor');
   this.phrase = phrase.toLowerCase();
 }
 
-   addPhraseToDisplay(){
-     const randArr = this.phrase.split("");
-     const map1 = randArr.map( function (letter) {
-       if (letter === ' '){
-         return `<li class="space">${letter}</li>`
-       } else { return `<li class="hide letter ${letter}">${letter}</li>`;
-     }
-     });
-     let noCommas = map1.join('');
-     let textSpot = document.getElementById('phrase').getElementsByTagName('ul')[0];
-     textSpot.textContent = '';
-     textSpot.insertAdjacentHTML("afterbegin", noCommas);
+
+addPhraseToDisplay(){
+ const randArr = this.phrase.split("");
+ const map1 = randArr.map(function (letter) {
+   if (letter === ' '){
+     return `<li class="space">${letter}</li>`
+   } else { return `<li class="hide letter ${letter}">${letter}</li>`;
+ }
+ });
+ let noCommas = map1.join('');
+ textSpot.textContent = '';
+ textSpot.insertAdjacentHTML("afterbegin", noCommas);
 }
 
-   checkLetter(){
+/**
+* Checks if passed letter is in phrase
+* @param (string) letter - Letter to check
+*/
+checkLetter(letter){
+console.log('check letter');
+const currentPhrase = game.activePhrase.phrase;
+	const letterMatch = currentPhrase.match(letter);
+	if (letterMatch === null) {
+      return false;
+    } else {
+      return true;
+    this.activePhrase.showMatchedLetter(letter);
+  }
+}
 
+showMatchedLetter(letter){
+let showLetter = document.getElementById('phrase').firstElementChild.children
+for (let i=0; i<showLetter.length; i++){
+  if (letter === showLetter[i].innerHTML) {
+    showLetter[i].className = `show letter ${letter}`;
+    }
+  }
+}
 
-   }
-
-   showMatchedLetter(){
-
-
-
-   }
-
-
-
-
- }
+}
